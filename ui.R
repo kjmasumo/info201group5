a414041d763c3aa8a1316ca9894597823e3ddb4a
KJMEUA <kjmasumo@uw.edu>
2018-05-26 22:47:10 -0700

    Got rough version of search function working

--- ui.R
+++ ui.R
@@ -1,5 +1,19 @@
 ui <- fluidPage(
-  
+  titlePanel("Car Safety Ratings"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        'year',
+        label = "Choose a year",
+        choices = years
+      ),
+      uiOutput('make_choice'),
+      uiOutput('model_choice')
+    ),
+    mainPanel(
+      tableOutput('chosen_vehicle_table')
+    )
+  )
 )
 
 shinyUI(ui)
